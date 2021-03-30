0e0306f85b7a378e5807c17f1541f67d39440caa
André <andreglock@gmail.com>
2021-03-30 16:48:25 +0200

    corrected Q9

--- start.js
+++ start.js
@@ -32,7 +32,7 @@ console.log('Q8: the volume of the cube is %s cm³', volumeCube);
 
 const bills = [22.35, 26.67, 35.92];
 
-console.log('Q9:  Bill 1: €%s\n     Bill 2: €%s\n     Bill 3: €%s', (bills[0] * 1.1).toFixed(2), (bills[1] * 1.1).toFixed(2), (bills[2] * 1.1).toFixed(2));
+console.log('Q9:  Bill 1: €%s\n     Bill 2: €%s\n     Bill 3: €%s', (bills[0] * 1.1).toFixed(2), (bills[1] * 1.15).toFixed(2), (bills[2] * 1.2).toFixed(2));
 
 const hoursAvrg = (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4)/ 10;
 
